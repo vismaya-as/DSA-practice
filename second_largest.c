8a953dc2ad61d3f9ce844fda81e945ebbb40bf3a
vismaya-as <nairvismaya3@gmail.com>
2026-07-12 17:24:53 +0530

    Create second_largest.c

---
+++ second_largest.c
@@ -0,0 +1,32 @@
+    #include <stdio.h>
+    #include<limits.h>
+    int main() {
+    int s,i;
+    printf("Enter size of array:");
+    scanf("%d",&s);
+    int ar[s];
+    for(i=0;i<s;i++)
+    {
+        printf("Enter array element %d:",i+1);
+        scanf("%d",&ar[i]);
+    }
+    int max=ar[0];
+    for(i=1;i<s;i++)
+    {
+        if(max<ar[i])
+        {
+            max=ar[i];
+        }
+    }
+    int max2=INT_MIN;
+    for(i=0;i<s;i++)
+    {
+        if(ar[i]>max2&&ar[i]<max&&ar[i]!=max)
+        {
+            max2=ar[i];
+        }
+    }
+   
+    printf("Second Largest Element:%d",max2);
+    return 0;
+}
